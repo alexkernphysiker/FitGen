3dcca0c329882fa6889b34c1288b18b2b6bdabe6
alexkernphysiker <kernphysiker@openmailbox.org>
2015-03-16 17:42:07 +0100

    example update

--- Example/equation.cpp
+++ Example/equation.cpp
@@ -5,14 +5,11 @@
 #include <additional_mutations.h>
 using namespace std;
 using namespace Fit;
-using namespace Fit::wrap_func_indexer;
+double Rosenbrock(ParamSet &X){
+	return pow(1.0-X[0],2)+100*pow(X[1]-pow(X[0],2),2);
+}
 int main(int argcnt, char **arg){
-	auto solve=Solve<SearchMin<
-		add<
-			power< sub<iconst<1>,par<0>>, iconst<2>>,
-			mul<iconst<100>, power< sub< par<1>, power<par<0>,iconst<2>> >, iconst<2> > >
-		>
-	>>();
+	auto solve=Solve<SearchMin<Rosenbrock>>();
 	solve->Init(100,make_shared<GenerateUniform>()<<make_pair(-80.0,80.0)<<make_pair(-80.0,80.0));
 	printf("Population size: %i \n%i variables\n",solve->PopulationSize(),solve->ParamCount());
 	while(!solve->ConcentratedInOnePoint())
