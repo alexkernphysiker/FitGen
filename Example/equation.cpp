e5adb836364d5e72acbefe3292480e376666e736
alexkernphysiker <kernphysiker@openmailbox.org>
2015-04-14 09:00:24 +0200

    Example update

--- Example/equation.cpp
+++ Example/equation.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <equation.h>
-#include <genetic.h>
 #include <initialconditions.h>
 using namespace std;
 using namespace Genetic;
-double Rosenbrock(double x, double y){
-	return pow(1.0-x,2)+100*pow(y-pow(x,2),2);
-}
 int main(int argcnt, char **arg){
-	Equation<DifferentialMutations<>> solve([](ParamSet&X){return Rosenbrock(X[0],X[1]);});
+	Equation<DifferentialMutations<>> solve([](ParamSet&X){
+		return pow(1.0-X[0],2)+100*pow(X[1]-pow(X[0],2),2);
+	});
 	solve.Init(100,make_shared<GenerateUniform>()<<make_pair(-50,50)<<make_pair(-50,50));
 	printf("Population size: %i \n%i variables\n",solve.PopulationSize(),solve.ParamCount());
 	while(!solve.ConcentratedInOnePoint()){
