4ecf3c9aa210279332ec81046bdb964f9a2d93a8
alexkernphysiker <kernphysiker@openmailbox.org>
2015-05-06 15:08:06 +0200

    GeneticException

--- genetic_exception.cpp
+++ genetic_exception.cpp
@@ -5,11 +5,9 @@ function<void(string)> output=[](string){};
 void SetGeneticErrorOutput(function<void(string)> func){
 	output=func;
 }
-
 GeneticException::GeneticException(std::string msg):exception(){
 	m_msg=msg;
 	output(msg);
-	printf("\nThrowing GeneticException: %s\n",msg.c_str());
 }
 const char* GeneticException::what() const throw(){
 	return m_msg.c_str();
