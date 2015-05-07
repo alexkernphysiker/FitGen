d6ce8534f9bed36b8c5f5fee841a8ecb336f78f0
alexkernphysiker <kernphysiker@openmailbox.org>
2015-05-07 11:14:56 +0200

    ParameterFunction test

--- Test/fit.cpp
+++ Test/fit.cpp
@@ -2,3 +2,11 @@
 #include <fit.h>
 using namespace Genetic;
 using namespace std;
+TEST(ParameterFunction,Basic){
+	int c=0;
+	double res=2;
+	ParameterFunction F([&c,&res](ParamSet&,ParamSet&){c++;return res;});
+	ParamSet X,P;
+	EXPECT_EQ(res,F(X,P));
+	EXPECT_EQ(1,c);
+}
