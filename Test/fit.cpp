90fab64b298a746a1d074b77cc03a884987aeec4
alexkernphysiker <kernphysiker@openmailbox.org>
2015-05-07 11:43:33 +0200

    FitPoints::Point test

--- Test/fit.cpp
+++ Test/fit.cpp
@@ -10,3 +10,32 @@ TEST(ParameterFunction,Basic){
 	EXPECT_EQ(res,F(X,P));
 	EXPECT_EQ(1,c);
 }
+typedef FitPoints::Point point;
+TEST(point,Base){
+	point P;
+	EXPECT_EQ(0,P.X.Count());
+	EXPECT_EQ(0,P.WX.Count());
+	P.y=1;
+	P.wy=1;
+	P.X<<1;
+	P.WX<<0.1;
+	{
+		point P2(P);
+		EXPECT_EQ(P.y,P2.y);
+		EXPECT_EQ(P.wy,P2.wy);
+		EXPECT_EQ(P.X.Count(),P2.X.Count());
+		EXPECT_EQ(P.X[0],P2.X[0]);
+		EXPECT_EQ(P.WX.Count(),P2.WX.Count());
+		EXPECT_EQ(P.WX[0],P2.WX[0]);
+	}
+	{
+		point P2=P;
+		EXPECT_EQ(P.y,P2.y);
+		EXPECT_EQ(P.wy,P2.wy);
+		EXPECT_EQ(P.X.Count(),P2.X.Count());
+		EXPECT_EQ(P.X[0],P2.X[0]);
+		EXPECT_EQ(P.WX.Count(),P2.WX.Count());
+		EXPECT_EQ(P.WX[0],P2.WX[0]);
+	}
+}
+
