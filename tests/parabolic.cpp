7dbd4e063f9a40ea2c4e9e3b610f77e3a444a73d
alexkernphysiker <kernphysiker@openmailbox.org>
2016-12-04 15:32:05 +0100

    submodule update

--- tests/parabolic.cpp
+++ tests/parabolic.cpp
@@ -23,34 +23,40 @@ public:
 TEST(ParabolicErrorEstimationFromChisq,Base){
 	ParabolicTest gen;
 	gen.SetUncertaintyCalcDeltas({0.01})
-	.Init(1,make_shared<InitialDistributions>()<<make_shared<RandomValueGenerator<double>>(-0.0001,0.0001),engine);
+	.Init(1,make_shared<InitialDistributions>()
+	    <<make_shared<RandomValueGenerator<double>>(-0.0001,0.0001)
+	,engine);
 	EXPECT_TRUE(pow(gen.ParametersWithUncertainties()[0].uncertainty()-1.0,2)<0.0001);
 }
 TEST(ParabolicErrorEstimationFromChisq,NegDelta){
 	ParabolicTest gen;
 	gen.SetUncertaintyCalcDeltas({-0.01})
-	.Init(1,make_shared<InitialDistributions>()<<make_shared<RandomValueGenerator<double>>(-0.0001,0.0001),engine);
+	.Init(1,make_shared<InitialDistributions>()
+	    <<make_shared<RandomValueGenerator<double>>(-0.0001,0.0001)
+	,engine);
 	EXPECT_THROW(gen.ParametersWithUncertainties()[0],Exception<ParabolicErrorEstimationFromChisq>);
 }
 TEST(ParabolicErrorEstimationFromChisq,NegZero){
 	ParabolicTest gen;
 	gen.SetUncertaintyCalcDeltas({0})
-	.Init(1,make_shared<InitialDistributions>()<<make_shared<RandomValueGenerator<double>>(-0.0001,0.0001),engine);
+	.Init(1,make_shared<InitialDistributions>()
+	    <<make_shared<RandomValueGenerator<double>>(-0.0001,0.0001)
+	,engine);
 	EXPECT_THROW(gen.ParametersWithUncertainties()[0],Exception<ParabolicErrorEstimationFromChisq>);
 }
 TEST(ParabolicErrorEstimationFromChisq,BaseTest){
-	for(int count=1;count<10;count++){
-		ParabolicTest gen;
-		auto init=make_shared<InitialDistributions>();
-		for(int i=0;i<count;i++)
-			init<<make_shared<RandomValueGenerator<double>>(-0.001,0.001);
-		gen.SetUncertaintyCalcDeltas(parEq(count,0.01)).Init(1,init,engine);
-		ASSERT_EQ(count,gen.ParametersWithUncertainties().size());
-		for(size_t i=0;i<gen.ParamCount();i++){
-			EXPECT_EQ(gen.Parameters()[i],gen.ParametersWithUncertainties()[i].val());
-			EXPECT_TRUE(pow(gen.ParametersWithUncertainties()[i].uncertainty()-1.0,2)<0.0001);
-		}
+    for(int count=1;count<10;count++){
+	ParabolicTest gen;
+	auto init=make_shared<InitialDistributions>();
+	for(int i=0;i<count;i++)
+	    init<<make_shared<RandomValueGenerator<double>>(-0.001,0.001);
+	gen.SetUncertaintyCalcDeltas(parEq(count,0.01)).Init(1,init,engine);
+	ASSERT_EQ(count,gen.ParametersWithUncertainties().size());
+	for(size_t i=0;i<gen.ParamCount();i++){
+	    EXPECT_EQ(gen.Parameters()[i],gen.ParametersWithUncertainties()[i].val());
+	    EXPECT_TRUE(pow(gen.ParametersWithUncertainties()[i].uncertainty()-1.0,2)<0.0001);
 	}
+    }
 }
 TEST(ParabolicErrorEstimationFromChisq,MoreInteresting){
 	for(int count=1;count<10;count++){
