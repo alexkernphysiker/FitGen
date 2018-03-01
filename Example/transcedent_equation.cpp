effae7c406596579190fba94e6f4d6ec84e1c6c0
rundel <alexrndl@gmail.com>
2018-03-01 13:50:50 +0200

    new example

---
+++ Example/transcedent_equation.cpp
@@ -0,0 +1,28 @@
+
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <Genetic/equation.h>
+#include <Genetic/initialconditions.h>
+#include <Genetic/genetic.h>
+using namespace std;
+using namespace Genetic;
+int main()
+{
+    //declaring
+    EquationSolver<DifferentialMutations<>> solution {
+        {
+            .left = [](const ParamSet & X){return sin(X[0]/2.0);},
+            .right = [](const ParamSet & X){return exp(-X[0]);}
+        }
+    };
+    //searching the solution
+    solution.Init(7, make_shared<InitialDistributions>()<< make_shared<DistribUniform>(-2,2));
+    while (!solution.AbsoluteOptimalityExitCondition(0.000000001))solution.Iterate();
+    //output results
+    cout << endl << "Solution: x=" << solution.Parameters()[0]<< endl;
+    cout << endl << "Sigma: " << solution.ParametersStatistics()[0].uncertainty()<< endl;
+    return 0;
+}
+
+
